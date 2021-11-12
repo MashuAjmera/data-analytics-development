126b0ea947447eeba5daf844a123af255e4fee6e
Mashu Ajmera <mashuajmera@gmail.com>
2021-11-12 16:11:47 +0530

    card for each endpoint

---
+++ src/components/endpoint.component.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { Card } from 'antd';
+import { DeleteOutlined } from '@ant-design/icons';
+
+export default class Endpoint extends Component {
+  state = {name:null}
+
+  componentDidMount(){
+    // fetch('/api/endpoints/'+this.props.endpoint.id)
+    // .then(data=>this.setState({"name":data.name}))
+    // .catch(error=>console.log(error))
+    this.setState({"name":"MQTT"})
+  }
+
+  render() {
+    const genExtra = () => (
+      <DeleteOutlined
+        onClick={event => {
+          // If you don't want click extra trigger collapse, you can prevent this:
+          event.stopPropagation();
+        }}
+      />
+    );
+    return (
+      <Card size="small" title={this.state.name} extra={genExtra()} style={{ width: 300, boxShadow:"0px 0px 4px #cccccc",  background: "#f0f2f5" }} bordered={false}>
+        {this.props.properties.map(property => <p>{property.name}: {property.value}</p>)}
+      </Card>
+    );
+  }
+}
