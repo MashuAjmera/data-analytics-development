507d3fef128c14027ccf8bb9e05efc07975ef3c4
Mashu Ajmera <mashuajmera@gmail.com>
2021-11-12 16:11:56 +0530

    card for each drive

---
+++ src/components/drive.component.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { Card } from 'antd';
+import { DeleteOutlined } from '@ant-design/icons';
+
+export default class Drive extends Component {
+  state = {name:null}
+
+  componentDidMount(){
+    // Use id to get Name
+    // fetch('/api/endpoints/'+this.props.endpoint.id)
+    // .then(data=>this.setState({"name":data.name}))
+    // .catch(error=>console.log(error))
+    this.setState({"name":"ACS880"})
+  }
+
+  render() {
+    const genExtra = () => (
+      <DeleteOutlined
+        onClick={event => {
+          // If you don't want click extra trigger collapse, you can prevent this:
+          event.stopPropagation();
+        }}
+      />
+    );
+    return (
+      <Card size="small" title={this.state.name} extra={genExtra()} style={{ width: 300, boxShadow:"0px 0px 4px #cccccc",  background: "#f0f2f5" }} bordered={false}>
+        {this.props.properties.map(property => <p>{property.name}: {property.value}</p>)}
+      </Card>
+    );
+  }
+}
