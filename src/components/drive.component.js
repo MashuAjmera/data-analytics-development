bdbf697a1f53c2d68465c81da2af0463d3d28d89
Mashu Ajmera <mashuajmera@gmail.com>
2021-11-17 12:33:25 +0530

    code enhancement

--- src/components/drive.component.js
+++ src/components/drive.component.js
@@ -1,30 +1,44 @@
 import React, { Component } from "react";
-import { Card } from 'antd';
-import { DeleteOutlined } from '@ant-design/icons';
+import { Card } from "antd";
+import { DeleteOutlined } from "@ant-design/icons";
 
 export default class Drive extends Component {
-  state = {name:null}
+  state = { name: null };
 
-  componentDidMount(){
-    // Use id to get Name
+  componentDidMount() {
+    // FETCH uses id to get Name GET /api/drives/<id>
     // fetch('/api/endpoints/'+this.props.endpoint.id)
     // .then(data=>this.setState({"name":data.name}))
     // .catch(error=>console.log(error))
-    this.setState({"name":"ACS880"})
+    this.setState({ name: "ACS880" });
   }
 
   render() {
     const genExtra = () => (
       <DeleteOutlined
-        onClick={event => {
+        onClick={(event) => {
           // If you don't want click extra trigger collapse, you can prevent this:
           event.stopPropagation();
         }}
       />
     );
     return (
-      <Card size="small" title={this.state.name} extra={genExtra()} style={{ width: 300, boxShadow:"0px 0px 4px #cccccc",  background: "#f0f2f5" }} bordered={false}>
-        {this.props.properties.map(property => <p>{property.name}: {property.value}</p>)}
+      <Card
+        size="small"
+        title={this.state.name}
+        extra={genExtra()}
+        style={{
+          width: 300,
+          boxShadow: "0px 0px 4px #cccccc",
+          background: "#f0f2f5",
+        }}
+        bordered={false}
+      >
+        {this.props.properties.map((property) => (
+          <p>
+            {property.name}: {property.value}
+          </p>
+        ))}
       </Card>
     );
   }
--- src/components/endpoint.component.js
+++ src/components/endpoint.component.js
@@ -1,29 +1,43 @@
 import React, { Component } from "react";
-import { Card } from 'antd';
-import { DeleteOutlined } from '@ant-design/icons';
+import { Card } from "antd";
+import { DeleteOutlined } from "@ant-design/icons";
 
 export default class Endpoint extends Component {
-  state = {name:null}
+  state = { name: null };
 
-  componentDidMount(){
-    // fetch('/api/endpoints/'+this.props.endpoint.id)
+  componentDidMount() {
+    // fetch('/api/endpoints/'+this.props.endpoint.id) GET /api/endpoints/<id>
     // .then(data=>this.setState({"name":data.name}))
     // .catch(error=>console.log(error))
-    this.setState({"name":"MQTT"})
+    this.setState({ name: "MQTT" });
   }
 
   render() {
     const genExtra = () => (
       <DeleteOutlined
-        onClick={event => {
+        onClick={(event) => {
           // If you don't want click extra trigger collapse, you can prevent this:
           event.stopPropagation();
         }}
       />
     );
     return (
-      <Card size="small" title={this.state.name} extra={genExtra()} style={{ width: 300, boxShadow:"0px 0px 4px #cccccc",  background: "#f0f2f5" }} bordered={false}>
-        {this.props.properties.map(property => <p>{property.name}: {property.value}</p>)}
+      <Card
+        size="small"
+        title={this.state.name}
+        extra={genExtra()}
+        style={{
+          width: 300,
+          boxShadow: "0px 0px 4px #cccccc",
+          background: "#f0f2f5",
+        }}
+        bordered={false}
+      >
+        {this.props.properties.map((property) => (
+          <p>
+            {property.name}: {property.value}
+          </p>
+        ))}
       </Card>
     );
   }
