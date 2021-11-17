f2905adc147234762950c045f42fcb8d82a271e9
Mashu Ajmera <mashuajmera@gmail.com>
2021-11-17 12:31:55 +0530

    on empty UI addded

--- src/components/client.component.js
+++ src/components/client.component.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Row, Col, Divider } from "antd";
+import { Row, Col, Divider, Button, Empty } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
 import Endpoint from "./endpoint.component";
 import Drive from "./drive.component";
 import AddDrive from "./addDrive.component";
@@ -9,7 +10,7 @@ export default class Client extends Component {
   state = { client: null, driveModel: false };
 
   componentDidMount() {
-    // get client information using client id
+    // FETCH client information using client id GET /api/clients/<id>
     this.setState({
       client: {
         id: "1234",
@@ -21,7 +22,7 @@ export default class Client extends Component {
           {
             id: "123",
             properties: [
-              { name: "host:", value: "localhost:1899" },
+              { name: "host", value: "localhost:1899" },
               { name: "protocol", value: "tcp" },
             ],
           },
@@ -37,36 +38,64 @@ export default class Client extends Component {
   render() {
     return this.state.client ? (
       <>
-        <Divider orientation="left">Endpoints</Divider>
-        <Row gutter={16}>
-          <Col span={5}>
-            <AddEndpoint />
+        <Row>
+          <Col span={12}>
+            <Divider orientation="left">Endpoints</Divider>
           </Col>
-          {this.state.client.endpoints.map((endpoint) => (
-            <Col span={5}>
-              <Endpoint
-                id={endpoint.id}
-                properties={endpoint.properties}
-                key={endpoint.id}
-              />
-            </Col>
-          ))}
-        </Row>
-        <Divider orientation="left">Drives</Divider>
-        <Row gutter={16}>
-          <Col span={5}>
-            <AddDrive />
+          <Col span={12}>
+            <Divider orientation="right">
+              <AddEndpoint />
+            </Divider>
           </Col>
-          {this.state.client.drives.map((drive) => (
-            <Col span={5}>
-              <Drive
-                id={drive.id}
-                properties={drive.properties}
-                key={drive.id}
-              />
-            </Col>
-          ))}
         </Row>
+        {this.state.client.endpoints ? (
+          <Row gutter={16}>
+            {this.state.client.endpoints.map((endpoint) => (
+              <Col span={5}>
+                <Endpoint
+                  id={endpoint.id}
+                  properties={endpoint.properties}
+                  key={endpoint.id}
+                />
+              </Col>
+            ))}
+          </Row>
+        ) : (
+          <Empty description={<span>No endpoint added yet!</span>}>
+            <Button type="primary" icon={<PlusOutlined />}>
+              Add Endpoint
+            </Button>
+          </Empty>
+        )}
+        <Row>
+          <Col span={12}>
+            <Divider orientation="left">Drives</Divider>
+          </Col>
+          <Col span={12}>
+            <Divider orientation="right">
+              <AddDrive />
+            </Divider>
+          </Col>
+        </Row>
+        {this.state.client.drives ? (
+          <Row gutter={16}>
+            {this.state.client.drives.map((drive) => (
+              <Col span={5}>
+                <Drive
+                  id={drive.id}
+                  properties={drive.properties}
+                  key={drive.id}
+                />
+              </Col>
+            ))}
+          </Row>
+        ) : (
+          <Empty description={<span>No drive added yet!</span>}>
+            <Button type="primary" icon={<PlusOutlined />}>
+              Add Drive
+            </Button>
+          </Empty>
+        )}
       </>
     ) : (
       <></>
