252eb0ba76fbcc3d8df38a7156043d51b7c862df
Mashu Ajmera <mashuajmera@gmail.com>
2021-11-18 14:57:33 +0530

    main collection

--- mongo.py
+++ mongo.py
@@ -2,9 +2,15 @@ import pymongo
 
 myclient = pymongo.MongoClient("mongodb+srv://Ashwin:Hackathonmongo@ashwinhackathon.u0vht.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", serverSelectionTimeoutMS=9999)
 # print(myclient.server_info())
-mydb = myclient["testdb"]
-mycol = mydb["testdata"]
+# mydb = myclient["testdb"]
+# mycol = mydb["testdata"]
 
-x = mycol.find_one()
+
+mydb = myclient["userData"]
+mycol = mydb["users"]
+x = mycol.find()
+
+for post in x:
+    print(post)
 
 print(x)
