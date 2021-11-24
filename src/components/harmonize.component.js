7e3a31a1badbee6e0b3ad835ff38217b60aed8be
Mashu Ajmera <mashuajmera@gmail.com>
2021-11-25 04:03:01 +0530

    harmonize without file

--- src/components/harmonize.component.js
+++ src/components/harmonize.component.js
@@ -1,54 +1,53 @@
 import React, { Component } from "react";
-import { message, Row, Col } from "antd";
+import { message, Row, Col, Typography } from "antd";
 import Drives from "./drives.component";
 import DataPoints from "./dataPoints.component";
 
 export default class Harmonize extends Component {
-  state = {
-    loadNext: false,
-    drive: null,
-  };
+  state = { drive: null };
 
-  handleClick = (id) => {
-    this.setState({ loadNext: true});
+  handleClick = (_id) => {
     const token = localStorage.getItem("Authorization");
     if (token) {
-    fetch(`/api/drives/${id}`, {
-      headers: { Authorization: token },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        this.setState({ drive: data.drive, loadNext: false });
+      fetch(`/api/drives/${_id}`, {
+        headers: { Authorization: token },
       })
-      .catch((error) => message.warning({ content: error }));
-    // let data = [];
-    // for (let i = 0; i < 46; i++) {
-    //   data.push({
-    //     key: i,
-    //     name: `Analog Input ${i}`,
-    //     unit: 'kmph',
-    //     type: 'uint',
-    //     interval: 1000,
-    //   });
-    // }
-    // this.setState({
-    //   drive: {
-    //     name: "ACS880",
-    //     id: "1234",
-    //     protocols: [
-    //       { id: "123", name: "ModBus" },
-    //       { id: "456", name: "OPCUA" },
-    //     ],
-    //     parameters:data
-    //   },
-    //   current: 1,
-    // });
+        .then((response) => response.json())
+        .then((data) => {
+          this.setState({ drive: data.drive });
+        })
+        .catch((error) => message.warning({ content: error }));
+      // let data = [];
+      // for (let i = 0; i < 46; i++) {
+      //   data.push({
+      //     key: i,
+      //     name: `Analog Input ${i}`,
+      //     unit: 'kmph',
+      //     type: 'uint',
+      //     interval: 1000,
+      //   });
+      // }
+      // this.setState({
+      //   drive: {
+      //     name: "ACS880",
+      //     id: "1234",
+      //     protocols: [
+      //       { id: "123", name: "ModBus" },
+      //       { id: "456", name: "OPCUA" },
+      //     ],
+      //     parameters:data
+      //   },
+      //   current: 1,
+      // });
     }
   };
 
   render() {
-
     const columns = [
+      {
+        title: "ParamId",
+        dataIndex: "_id",
+      },
       {
         title: "Name",
         dataIndex: "name",
@@ -61,14 +60,34 @@ export default class Harmonize extends Component {
     ];
 
     return (
-      <Row gutter={16}>
-        <Col span={6}>
-          <Drives handleClick={this.handleClick}/>
-        </Col>
-        <Col span={18}>
-          <DataPoints columns={columns} dataSource={this.state.drive && this.state.drive.parameters}/>
-        </Col>
-      </Row>
+      <>
+        <Typography.Title level={2} style={{ textAlign: "center" }}>
+          Harmonize Drive Parameters
+        </Typography.Title>
+        <Row gutter={16}>
+          <Col span={6}>
+            <div
+              className="site-layout-background"
+              style={{ padding: 24, backgroundColor: "white", height: "35rem" }}
+            >
+              <Drives handleClick={this.handleClick} />
+            </div>
+          </Col>
+          <Col span={18}>
+            <div
+              className="site-layout-background"
+              style={{ padding: 24, backgroundColor: "white", height: "35rem" }}
+            >
+              <DataPoints
+                columns={columns}
+                handleClick={this.handleClick}
+                _id={this.state.drive && this.state.drive._id}
+                dataSource={this.state.drive && this.state.drive.parameters}
+              />
+            </div>
+          </Col>
+        </Row>
+      </>
     );
   }
 }
