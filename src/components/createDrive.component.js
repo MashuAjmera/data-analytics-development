f28979d126e9aba5ecf8ab080e69a10a6ee0cb32
Mashu Ajmera <mashuajmera@gmail.com>
2021-11-24 14:06:26 +0530

    protocols loaded dnamically and multiple can be selected

--- src/components/createDrive.component.js
+++ src/components/createDrive.component.js
@@ -1,107 +1,136 @@
 import React, { Component } from "react";
-import {
-  Form, Select,Button, Upload, message, Typography
-} from "antd";
+import { Form, Select, Button, Upload, message, Typography } from "antd";
 import { UploadOutlined } from "@ant-design/icons";
 export default class CreateDrive extends Component {
-  state={protocols:[{"id":"1234","name":"Modbus"},{"id":"5678","name":"OpcUA"}]}
+  state = {
+    protocols: [],
+    selectedItems: [],
+    loadProtocols:false
+  };
+  componentDidMount(){
+    const token = localStorage.getItem("Authorization");
+    if(token){
+      this.setState({loadProtocols:true});
+      // FETCH name of all the drives GET /api/drives
+      fetch("/api/protocols/", {
+        headers: { Authorization: token },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          this.setState({ loadProtocols: false, protocols: data.protocols });
+        })
+        .catch((error) => message.warning({ content: error }));
+  }
+}
+
+  handleChange = (selectedItems) => {
+    this.setState({ selectedItems });
+  };
 
   render() {
     const onFinish = (values) => {
-    console.log('Success:', values);
-  };
+      console.log("Success:", values);
+    };
 
-  const onFinishFailed = (errorInfo) => {
-    console.log('Failed:', errorInfo);
-  };
-  const props = {
-    name: 'file',
-    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
-    headers: {
-      authorization: 'authorization-text',
-    },
-    onChange(info) {
-      if (info.file.status !== 'uploading') {
-        console.log(info.file, info.fileList);
-      }
-      if (info.file.status === 'done') {
-        message.success(`${info.file.name} file uploaded successfully`);
-      } else if (info.file.status === 'error') {
-        message.error(`${info.file.name} file upload failed.`);
-      }
-    },
-  };
-  const { Title } = Typography;
+    const onFinishFailed = (errorInfo) => {
+      console.log("Failed:", errorInfo);
+    };
+
+    const props = {
+      name: "file",
+      action: "https://www.mocky.io/v2/5cc8019d300000980a055e76",
+      headers: {
+        authorization: "authorization-text",
+      },
+      onChange(info) {
+        if (info.file.status !== "uploading") {
+          console.log(info.file, info.fileList);
+        }
+        if (info.file.status === "done") {
+          message.success(`${info.file.name} file uploaded successfully`);
+        } else if (info.file.status === "error") {
+          message.error(`${info.file.name} file upload failed.`);
+        }
+      },
+    };
+    const { Title } = Typography;
+    const { selectedItems } = this.state;
+    const filteredOptions = this.state.protocols.filter(
+      (protocol) => !selectedItems.includes(protocol)
+    );
+
     return (
       <Form
-      name="basic"
-      labelCol={{
-        span: 8,
-      }}
-      wrapperCol={{
-        span: 8,
-      }}
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-    >
-    <Title style={{ textAlign: "center" }}>Create your Virtual Drive</Title>
-      <Form.Item
-        label="Parameter File"
-        name="name"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your username!',
-          },
-        ]}
-      >
-        <Upload {...props}>
-    <Button icon={<UploadOutlined />}>Upload JSON</Button>
-  </Upload>
-      </Form.Item>
-
-      <Form.Item
-        label="Protocols"
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your password!',
-          },
-        ]}
-      >
-      <Select
-        showSearch
-        placeholder={`Select Protocols to Add`}
-        optionFilterProp="children"
-        filterOption={(input, option) =>
-          option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-        }
-        onChange={this.handleChange}
-      >
-        {this.state.protocols.map((section) => (
-          <Select.Option key={section.id} value={section.id}>
-            {section.name}
-          </Select.Option>
-        ))}
-      </Select>
-      </Form.Item>
-
-      <Form.Item
-        wrapperCol={{
-          offset: 8,
-          span: 16,
+        name="basic"
+        labelCol={{
+          span: 8,
         }}
+        wrapperCol={{
+          span: 8,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
       >
-        <Button type="primary" htmlType="submit">
-          Create Drive
-        </Button>
-      </Form.Item>
-    </Form>
+        <Title style={{ textAlign: "center" }}>Create your Virtual Drive</Title>
+        <Form.Item
+          label="Parameter File"
+          name="name"
+          rules={[
+            {
+              required: true,
+              message: "Please input your username!",
+            },
+          ]}
+        >
+          <Upload {...props}>
+            <Button icon={<UploadOutlined />}>Upload JSON</Button>
+          </Upload>
+        </Form.Item>
+
+        <Form.Item
+          label="Protocols"
+          name="protocols"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
+        >
+          <Select
+            mode="multiple"
+            value={selectedItems.map(p=>p.name)}
+            showSearch
+            placeholder={`Select Protocols to Add`}
+            optionFilterProp="children"
+            filterOption={(input, option) =>
+              option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+            }
+            onChange={this.handleChange}
+          >
+            {filteredOptions.map((section) => (
+              <Select.Option key={section._id} value={section._id}>
+                {section.name}
+              </Select.Option>
+            ))}
+          </Select>
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit">
+            Create Drive
+          </Button>
+        </Form.Item>
+      </Form>
     );
   }
 }
