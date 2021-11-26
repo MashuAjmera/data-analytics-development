9ff6e9f3c6687112d6f11f66c70a98f66e60e9d1
Mashu Ajmera <mashuajmera@gmail.com>
2021-11-26 11:48:00 +0530

    sidebar

--- src/components/gallery.component.js
+++ src/components/gallery.component.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Card, Col, Row, Avatar, Rate, Spin, message, Typography } from "antd";
+import { Card, Col, Row, Avatar, Rate, Spin, message, Typography, Layout, Divider, Input, Checkbox, Slider } from "antd";
 import { CloudDownloadOutlined, EllipsisOutlined } from "@ant-design/icons";
 import logo from "../static/favicon.png";
 
@@ -33,67 +33,73 @@ export default class AppGallery extends Component {
     //         </>
     //       ),
     //     },
-    //     {
-    //       avatar: <Avatar src="https://joeschmoe.io/api/v1/random" />,
-    //       title: "ABB Client",
-    //       description: (
-    //         <>
-    //           <p>endpoints: modbus, opcua</p>
-    //           <p>drives: ACS800</p>
-    //           <Rate disabled defaultValue={4} />
-    //         </>
-    //       ),
-    //     },
-    //     {
-    //       avatar: <Avatar src="https://joeschmoe.io/api/v1/random" />,
-    //       title: "ABB Client",
-    //       description: (
-    //         <>
-    //           <p>endpoints: modbus, opcua</p>
-    //           <p>drives: ACS800</p>
-    //           <Rate disabled defaultValue={4} />
-    //         </>
-    //       ),
-    //     },
-    //     {
-    //       avatar: <Avatar src="https://joeschmoe.io/api/v1/random" />,
-    //       title: "ABB Client",
-    //       description: (
-    //         <>
-    //           <p>endpoints: modbus, opcua</p>
-    //           <p>drives: ACS800</p>
-
-    //         </>
-    //       ),
-    //     },
     // ];
-    const { Text, Paragraph  } = Typography
-    return this.state.loadClients ?
-      <div className="example">
-        <Spin size="large" />
-      </div> :
-      <div className="site-card-wrapper">
-        <Row gutter={64} justify="center">
-          {this.state.apps.map((d) => (
-            <Col span={6}>
-              <Card style={{ marginTop: 16 }}
-                actions={[
-                  <CloudDownloadOutlined key="download" />,
-                  <EllipsisOutlined key="ellipsis" />,
-                ]}>
-                <Card.Meta
-                  avatar={<Avatar src={logo} />}
-                  title={d.name}
-                  description={<>
-                    <Paragraph ><Text strong>endpoints</Text>: {d.endpoints.map(e => e + ", ")}<br/>
-                    <Text strong>drives</Text>: {d.drives.map(e => e + ", ")}</Paragraph >
-                    <Rate disabled defaultValue={4} />
-                  </>}
-                />
-              </Card>
-            </Col>
-          ))}
-        </Row>
-      </div>
+    const { Text, Paragraph } = Typography
+    const { Sider } = Layout;
+    return (<Layout>
+      <Sider width={300} className="site-layout-background" theme="light" style={{
+        padding: "20px", height: "80vh",
+        overflow: 'auto',
+        position: 'fixed',
+      }}>
+        <Input.Search placeholder="Search app name" enterButton />
+        <Divider orientation="left">Type</Divider>
+        <Checkbox.Group options={[
+          { label: 'Cloud', value: 'Apple' },
+          { label: 'Edge', value: 'Pear' },
+        ]} />
+        <Divider orientation="left">Endpoints</Divider>
+        <Checkbox.Group options={[
+          { label: 'MQTT', value: 'Apple' },
+          { label: 'IoT Hub', value: 'Pear' },
+        ]} />
+        <Divider orientation="left">Drives</Divider>
+        <Checkbox.Group defaultChecked={false} options={[
+          { label: 'ACS550', value: 'Apple' },
+          { label: 'ACH550', value: 'Pear' },
+          { label: 'ACS880', value: 'Orange' },
+          { label: 'ACD550', value: 'Pear1' },
+          { label: 'ACS800', value: 'Orange1' },
+          { label: 'ACS990', value: 'Pear2' },
+          { label: 'ACH950', value: 'Orange2' },
+        ]} />
+        <Divider orientation="left">Protocols</Divider>
+        <Checkbox.Group defaultChecked={false} options={[
+          { label: 'ModBus', value: 'Apple' },
+          { label: 'OPC UA', value: 'Pear' },
+          { label: 'Ethernet', value: 'Orange' },
+        ]} />
+        <Divider orientation="left">Data Points</Divider>
+        <Slider max={50} min={1} range defaultValue={[4, 12]} />
+        <Divider orientation="left">Average Rating</Divider>
+        <Rate allowHalf style={{}} />
+      </Sider>
+      <Layout style={{ padding: '0 24px 24px', marginLeft: 400, marginRight: 100 }}>
+        {this.state.loadClients ? <div className="example">
+          <Spin size="large" />
+        </div> :
+          <Row gutter={32} justify="center">
+            {this.state.apps.map((d) => (
+              <Col span={12}>
+                <Card style={{ marginTop: 16 }}
+                  actions={[
+                    <CloudDownloadOutlined key="download" />,
+                    <EllipsisOutlined key="ellipsis" />,
+                  ]}>
+                  <Card.Meta
+                    avatar={<Avatar src={logo} />}
+                    title={d.name}
+                    description={<>
+                      <Paragraph ><Text strong>endpoints</Text>: {d.endpoints.map(e => e + ", ")}<br />
+                        <Text strong>drives</Text>: {d.drives.map(e => e + ", ")}</Paragraph >
+                      <Rate disabled defaultValue={4} />
+                    </>}
+                  />
+                </Card>
+              </Col>
+            ))}
+          </Row>}
+      </Layout>
+    </Layout>)
   }
 }
